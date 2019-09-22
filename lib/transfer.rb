52af8f072e7002f09be654375a2c740af0fa3e26
Umberto Palazzo <upalazzo93@gmail.com>
2019-09-22 22:54:44 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,8 +19,9 @@ class Transfer
       @receiver.balance += @amount
       @status = "complete"
     else 
-      return "Transaction rejected. Please check your account balance."
       @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+      
     end 
   end 
   
