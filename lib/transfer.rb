00661379ad09503c620a0669c6df1c34d6bc7c67
Umberto Palazzo <upalazzo93@gmail.com>
2019-09-22 22:12:08 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -10,8 +10,11 @@ class Transfer
   end 
   
   def valid? 
-    sender.valid? && receiver.valid? 
+    @sender.valid? && @receiver.valid? 
   end 
   
+  def execute_transaction 
+    
+  end 
   
 end
