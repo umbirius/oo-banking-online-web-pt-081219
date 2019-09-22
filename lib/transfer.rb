a085c6672ba2fe05ab6fdf3acb0ca658741d9801
Umberto Palazzo <upalazzo93@gmail.com>
2019-09-22 22:56:36 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -28,7 +28,7 @@ class Transfer
     if @status == "complete"
       @sender.balance += @amount
       @receiver.balance -= @amount
-  @status = "reversed"
+      @status = "reversed"
     end 
   end 
 end
