3d00cae83f02bd218dd234c9727b288c40ab73aa
Umberto Palazzo <upalazzo93@gmail.com>
2019-09-22 23:13:30 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
   end 
   
   def execute_transaction 
-    if @status == "pending" && @sender.balance > @amount 
+    if @status == "pending" && @sender.balance > @amount && self.valid?
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
