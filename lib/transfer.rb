c217fcdaba1f625c5f21508d5eaf52fb3b942d1b
Umberto Palazzo <upalazzo93@gmail.com>
2019-09-22 22:09:36 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,11 +1,11 @@
 class Transfer
   
-  attr_accessor :sender, :receiver
+  attr_accessor :sender, :receiver, :status
   attr_writer 
   def initialize(sender, receiver, amt)
     @sender = sender
     @receiver = receiver
-    
+    @status = pending
   end 
   
 end
