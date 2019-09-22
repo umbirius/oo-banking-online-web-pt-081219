92ecceed5d445104bd8075728166e1603c1c0732
Umberto Palazzo <upalazzo93@gmail.com>
2019-09-22 22:00:05 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,12 +1,13 @@
 class BankAccount
   
   attr_accessor :name
-  attr_reader :balance
+  attr_reader :balance, status
   
 
   def initialize(name)
     @name = name 
     @balance = 1000
+    @status = open
   end 
   
 
