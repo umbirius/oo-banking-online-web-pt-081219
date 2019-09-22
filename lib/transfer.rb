77ac769372afd39ba131e229a448378d08d1e677
Umberto Palazzo <upalazzo93@gmail.com>
2019-09-22 22:14:29 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
   def execute_transaction 
     @sender.balance -= @amount
     @receiver.balance += @amount
-    @status = "complete"
+    self.status = "complete"
   end 
   
 end
